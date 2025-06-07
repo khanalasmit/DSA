3f9be62593c8deb683fca8e546c1cd9d06d177ed
khanalasmit <asmitpasa2063@gmail.com>
2025-06-08 00:14:54 +0545

    Added the readme file

---
+++ tower_of_honoi.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void Tower_of_honoi(int n, char src,char axu, char dest){
+    if (n==1){
+        cout<<"Move from"<<src<<" to"<< dest<<". \n";
+        return ;
+    }
+    return 
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the numbers of  disk for the tower of the honoi:";
+    cin>>n;
+    cout<<"Moves to opearte are:";
+
+}
