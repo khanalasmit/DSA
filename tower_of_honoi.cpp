29fb5efbbc39f5bd82aa7faa643677e991b04f2b
khanalasmit <asmitpasa2063@gmail.com>
2025-06-08 09:23:49 +0545

    completed the tower of honoi

--- tower_of_honoi.cpp
+++ tower_of_honoi.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 
 void Tower_of_honoi(int n, char src,char axu, char dest){
-    if (n==1){
-        cout<<"Move from"<<src<<" to"<< dest<<". \n";
+    if (n==0){
+        //cout<<"Move from"<<src<<" to"<< dest<<". \n";
         return ;
     }
-    return 
+    Tower_of_honoi(n-1,src,dest,axu);
+    cout<<"Move disk "<<n<<" from "<<src<<" to rod"<<dest<<endl;
+    Tower_of_honoi(n-1,axu,src,dest);
+
 }
 
 int main()
@@ -14,6 +17,7 @@ int main()
     int n;
     cout<<"Enter the numbers of  disk for the tower of the honoi:";
     cin>>n;
-    cout<<"Moves to opearte are:";
-
+    cout<<"Moves to opearte are:"<<endl;
+    Tower_of_honoi(n,'A','B','C');
+    return 0;
 }
